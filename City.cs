4e656d7f047608dea5a0ce8f57904d66bb8df5a1
Pablo Bilbao <pablobilbao1605@gmail.com>
2024-10-02 09:56:38 +0200

    PoliceStation added and city moddified

--- City.cs
+++ City.cs
@@ -2,6 +2,7 @@
 {
     class City: IMessageWritter
     {
-
+        private PoliceStation policeStation;
+        public List<Taxi> taxiLicenses
     }
 }
---
+++ PoliceStation.cs
@@ -0,0 +1,8 @@
+﻿namespace Practica1
+{
+    class PoliceStation : IMessageWritter
+    {
+        public List<PoliceCar> policeCars { get; private set; };
+        private bool detectedInfractor;
+    }
+}
